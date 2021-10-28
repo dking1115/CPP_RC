40ea6b07fbd06c5cfb919e9a1bb601869337142b
dking1115 <davidking@amdg.rockhursths.edu>
2021-10-28 13:41:21 -0500

    idk if this will work

--- Transcieve_test_pi.py
+++ Transcieve_test_pi.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import serial
 import time
+i=0
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        i += 1
+        ser.write(b + i + "\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
+        if i>100:
+            i=0
