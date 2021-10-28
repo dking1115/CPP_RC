b0e32aef21b0616fb1b494f129d05456f8bea133
dking1115 <davidking@amdg.rockhursths.edu>
2021-10-28 13:04:03 -0500

    TX test

---
+++ TX.py
@@ -0,0 +1,13 @@
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0',250000)
+i=0
+while True:
+    i += 1
+    if i>100:
+        i=-100
+    read_serial=ser.readline()
+	s[0] = str(int (ser.readline(),16))
+	print s[0]
+	print read_serial
+    ser.print(i)
