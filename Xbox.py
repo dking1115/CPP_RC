96cd40bca98ab8d2fe1e51cc4f5ef89c67a99bdd
dking1115 <davidking@amdg.rockhursths.edu>
2021-10-28 19:57:33 -0500

    trying to apply it

--- Xbox.py
+++ Xbox.py
@@ -1,9 +1,14 @@
 from inputs import devices
 from inputs import get_gamepad
+
 for device in devices:
     print(device)
 while 1:
     events = get_gamepad()
     for event in events:
-        print(event.ev_type)
+        #print(event.code)
+        if event.type="ABS_X":
+            x=event.state
+            print(x)
+
 
