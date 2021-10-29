7bbc12fe73396c8be48a3c8fa0a8eda337b8db74
dking1115 <davidking@amdg.rockhursths.edu>
2021-10-28 19:27:09 -0500

    testing xbox

--- Xbox.py
+++ Xbox.py
@@ -1,3 +1,7 @@
 from inputs import devices
 for device in devices:
     print(device)
+while 1:
+    events = get_gamepad()
+    for event in events:
+        print(event.ev_type, event.code, event.state)
