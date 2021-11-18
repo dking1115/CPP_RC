f8d36def2ad0c9c65a5bbf6a86e17c3faaf7a7e7
dking1115 <davidking@amdg.rockhursths.edu>
2021-11-17 19:59:36 -0600

    trying to get split to work

--- receive.py
+++ receive.py
@@ -8,7 +8,7 @@ can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socket
 
 #msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
 while True:
-    msg = can0.recv(10.0)
+    str(msg)= can0.recv(10.0)
     print (msg)
     print(msg.split())
 
