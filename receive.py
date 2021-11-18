c8b6a734816101ee7203ec166a29fe0883b714b2
dking1115 <davidking@amdg.rockhursths.edu>
2021-11-17 19:58:01 -0600

    indentation error

--- receive.py
+++ receive.py
@@ -8,9 +8,9 @@ can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socket
 
 #msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
 while True:
-msg = can0.recv(10.0)
-print (msg)
-print(msg.split())
+    msg = can0.recv(10.0)
+    print (msg)
+    print(msg.split())
 
 if msg is None:
     print('Timeout occurred, no message.')
