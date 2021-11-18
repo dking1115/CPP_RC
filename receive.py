db5b8813e0dc2ad641b571a36664a9f85352af36
dking1115 <davidking@amdg.rockhursths.edu>
2021-11-17 20:20:08 -0600

    changed bits and converting to int

--- receive.py
+++ receive.py
@@ -8,7 +8,7 @@ can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socket
 
 #msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
 while True:
-    msg= can0.recv(10.0)
+    msg= can0.recv(1.0)
     if msg is None:
         print('Timeout occurred, no message.')
         os.system('sudo ifconfig can0 down')
@@ -18,16 +18,16 @@ while True:
     txt=txt.split()
     #print(txt[3])
     ID=txt[3]
-    Bitone=txt[6]
-    Bittwo=txt[7]
-    Bitthree=txt[8]
-    Bitfour=txt[9]
-    Bitfive=txt[10]
-    Bitsix=txt[11]
-    Bitseven=txt[12]
-    Biteight=txt[13]
+    Bitone=txt[7]
+    Bittwo=txt[8]
+    Bitthree=txt[9]
+    Bitfour=txt[10]
+    Bitfive=txt[11]
+    Bitsix=txt[12]
+    Bitseven=txt[13]
+    Biteight=txt[14]
     if(ID=="0cfdd633"):
-        print(Bitone)
+        print(int(Bitone,16))
     #print(txt)
     
 
