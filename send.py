7d3c50045d4bfcd21d9a641f4633af82d01e8508
dking1115 <davidking@amdg.rockhursths.edu>
2022-04-13 12:32:44 -0500

    syntax

--- send.py
+++ send.py
@@ -5,7 +5,7 @@ can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socket
 
 while True:
     
-    for x in range 32768:
+    for x in range(32768):
         msx= x >> 8
         lsx= x & ((1 << 8) - 1)
         msg = can.Message(arbitration_id=0x123, data=[msx, lsx, 2, 3, 4, 5, 6, 7], extended_id=True)
