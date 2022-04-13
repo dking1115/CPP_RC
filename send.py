cdf65b40335af4b84fa62eff7e976328991adaac
dking1115 <davidking@amdg.rockhursths.edu>
2022-04-13 12:08:50 -0500

    Moved some stuff around

--- send.py
+++ send.py
@@ -1,12 +1,18 @@
 import os
 import can
 
-os.system('sudo ip link set can0 type can bitrate 250000')
-os.system('sudo ifconfig can0 up')
-
 can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socketcan_native
 
-msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=True)
-can0.send(msg)
+While True:
+    
+    for x in range 32768:
+        msx= x >> 8
+        lsx= x & ((1 << 8) - 1)
+        msg = can.Message(arbitration_id=0x123, data=[msx, lsx, 2, 3, 4, 5, 6, 7], extended_id=True)
+        can0.send(msg)
+        sleep (.01)
+
+
+
 
 os.system('sudo ifconfig can0 down')
