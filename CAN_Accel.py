7a2f95b8af6cc19b5bc8cec0b33a4519f919f99f
dking1115 <davidking@amdg.rockhursths.edu>
2022-04-13 02:05:37 -0500

    Accel was going out of range

--- CAN_Accel.py
+++ CAN_Accel.py
@@ -72,7 +72,7 @@ while True:
     # in meters per second squared):
     #x,y,z = bno.read_gravity()
     # Sleep for a second until the next reading.
-    msg = can.Message(arbitration_id=0x123, data=[abs(int(x*10)), abs(int(y*10)), abs(int(z*10)), abs(int(gx*10)), abs(int(gy*10)), abs(int(gz*10)), 6, 7], extended_id=True)
+    msg = can.Message(arbitration_id=0x123, data=[abs(int(x*5)), abs(int(y*5)), abs(int(z*5)), abs(int(gx*10)), abs(int(gy*10)), abs(int(gz*10)), 6, 7], extended_id=True)
     can0.send(msg)
     time.sleep(.01)
 
